2e7fdc6a7c81af5c45127bc2ee8f450538b751d8
Ayu-99 <51323447+Ayu-99@users.noreply.github.com>
2019-06-04 18:31:09 +0530

    Add files via upload

---
+++ session1.py
@@ -0,0 +1,28 @@
+#Creation of Containers in Python(creation statement-sth will happen in  memory)
+instagramUserName="auribises"
+time=3
+johnsAge=22
+
+#update data in container
+time=4
+johnsAge=30
+
+#delete operation in Python
+#del instagramUserName
+
+#Read container in Python
+print(instagramUserName)
+#print(id(instagramUserName))
+print(hex(id(instagramUserName)))
+#print(oct(id(instagramUserName)))
+#print(bin(id(instagramUserName)))
+
+print()
+
+print(time)
+print(hex(id(time)))
+
+print()
+
+print(johnsAge)
+print(hex(id(johnsAge)))
