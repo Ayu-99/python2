f94c185c1fe153c96336c0615e0731b71ebf68da
Ayu-99 <51323447+Ayu-99@users.noreply.github.com>
2019-06-04 18:33:25 +0530

    Add files via upload

---
+++ session1(e).py
@@ -0,0 +1,15 @@
+#Dictionary | JSON(Explor)
+dishes = {"dal":100, "paneer":200, "chicken":300}
+#dal is key which has its own value(100)
+
+print(dishes)
+print(hex(id(dishes)))
+print(type(dishes))
+print(dishes["dal"])
+
+
+#draw memory representation of dictionary
+
+#how to declare complex variable
+
+
