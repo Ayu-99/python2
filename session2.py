c430dccbb83f5a6387725c7a3f8149cc537d270e
Ayushi Sharma <51323447+Ayu-99@users.noreply.github.com>
2019-06-05 17:31:39 +0530

    Add files via upload

---
+++ session2.py
@@ -0,0 +1,23 @@
+# Operators
+# Arithmetic Operators
+
+dish1 = 100
+dish2 = 200
+bill = dish1 + dish2
+print("bill is:",bill)
+# -,*,/,//,**
+
+taxes= .05*bill
+print(taxes)
+
+totalBill= bill + taxes
+print("Bill with taxes is:\u20b9",totalBill)
+
+num1 = 2
+num2 = 8
+#num3= num1 / num2
+num3 = num1 ** num2
+print(num3)
+
+#num4 = num1 // num2
+#print(num4)
