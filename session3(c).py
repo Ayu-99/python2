c6b4846d50be4e7c17f9e908bc458891bfa9000c
Ayushi Sharma <51323447+Ayu-99@users.noreply.github.com>
2019-06-06 17:28:56 +0530

    Add files via upload

---
+++ session3(c).py
@@ -0,0 +1,19 @@
+#sets
+s={1,2,3,4}
+
+# 1.concatenation
+#print({s}+[5,6])
+
+# 2.repition
+#print({s}*2)
+
+#3.membership testing
+print(1 in s)
+
+
+#4.slicing
+#print(s[1:4])
+
+#5.indexing
+#print(s[3])
+
